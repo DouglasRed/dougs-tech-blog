5d3be8521e95bc994812c14d76700a4b994cf41e
Doug Red <douglasred63@yahoo.com>
2022-08-31 16:05:43 -0500

    added password protection and the ability to create and manipulate posts

--- models/User.js
+++ models/User.js
@@ -1,10 +1,15 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
-
+const bcrypt = require("bcrypt");
 // create our User model
-class User extends Model {}
+class User extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
-// create fields/columns for User model
+// Define table columns and configuration
 User.init(
   {
     id: {
@@ -34,6 +39,19 @@ User.init(
     },
   },
   {
+    hooks: {
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
+        return updatedUserData;
+      },
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
